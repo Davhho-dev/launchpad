f607602fa4911ea393e4ce630ec8b6893dc01323
Davhho-dev <Davhho@utexas.edu>
2022-09-27 22:57:12 -0500

    Style arrow buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const arrowContainer = document.querySelector(".arrow-container");
 for(let i = 0; i < 8; i++) {
     const arrowBtns = document.createElement("button");
-    arrowBtns.classList.add(`arrowBtns${i}`);
+    arrowBtns.classList.add("arrow", `arrowBtns${i}`);
+    arrowBtns.textContent = i;
     arrowContainer.append(arrowBtns);
 }
