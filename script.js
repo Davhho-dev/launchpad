28df036ae9b21566d7d071a203b75d16e41f13f7
Davhho-dev <Davhho@utexas.edu>
2022-09-27 23:55:20 -0500

    Layout the buttons to be evenly spaced

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ for(let i = 0; i < 8; i++) {
     arrowBtns.textContent = i;
     arrowContainer.append(arrowBtns);
 }
+
+const buttonContainer = document.querySelector(".button-container");
+for(let row = 0; row < 8; row++) {
+    for(let column = 0; column < 8; column++) {
+        const launchButton = document.createElement("button");
+        launchButton.classList.add("launch-button", `launchBtns-row${row}-column${column}`);
+        buttonContainer.append(launchButton);
+    }
+}
