c129c45876105d11f9171e298f1c73991fe68471
Davhho-dev <Davhho@utexas.edu>
2022-09-30 18:12:05 -0500

    Create daisy chain light effect when key press

--- script.js
+++ script.js
@@ -1,7 +1,80 @@
-const buttonArray = 
-  ["81", "87", "69", "82", "84", "89", "85", "73", "79", "80", "219", "221", "220", "65", "83", "68", "70", "71", "72", "74", "75", "76", "186", "222", "13", "90", "88", "67", "86", "66","78", "77", "188", "190", "191", "16"];
+const buttonArray = [
+  "81",
+  "87",
+  "69",
+  "82",
+  "84",
+  "89",
+  "85",
+  "73",
+  "79",
+  "80",
+  "219",
+  "221",
+  "220",
+  "65",
+  "83",
+  "68",
+  "70",
+  "71",
+  "72",
+  "74",
+  "75",
+  "76",
+  "186",
+  "222",
+  "13",
+  "90",
+  "88",
+  "67",
+  "86",
+  "66",
+  "78",
+  "77",
+  "188",
+  "190",
+  "191",
+  "16",
+];
 
-const buttonImg = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "Shift"];
+const buttonImg = [
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "[",
+  "]",
+  "\\",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  ";",
+  "'",
+  "Enter",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+  ",",
+  ".",
+  "/",
+  "Shift",
+];
 
 const arrowArray = ["left", "up", "right", "down"];
 const arrowImg = ["\u2190", "\u2191", "\u2192", "\u2193"];
@@ -25,7 +98,7 @@ for (let i = 0; i < 4; i++) {
 // }
 
 const buttonContainer = document.querySelector(".button-container");
-for(let i = 0; i < 36; i++) {
+for (let i = 0; i < 36; i++) {
   const launchButton = document.createElement("div");
   launchButton.classList.add("padKey", `padKey-${buttonArray[i]}`);
   launchButton.textContent = buttonImg[i];
@@ -33,16 +106,54 @@ for(let i = 0; i < 36; i++) {
 }
 
 window.addEventListener("keydown", (e) => {
-    let keySelected = e.keyCode;
-    const audio = document.querySelector(`audio[data-key="${keySelected}"]`);
-    audio.currentTime = 0;
-    audio.play();
-    console.log(e.keyCode);
-    const padSelected = document.querySelector(`div.padKey-${keySelected}`);
-    padSelected.classList.add('playing');
-    
-    setTimeout(function() {
-        padSelected.classList.remove("playing");
-    }, 100);
-})
+  let keySelected = e.keyCode;
+  const audio = document.querySelector(`audio[data-key="${keySelected}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  console.log(e.keyCode);
+  const padSelected = document.querySelector(`div.padKey-${keySelected}`);
+  padSelected.classList.add("playing");
+  setTimeout(function () {
+    padSelected.classList.remove("playing");
+    test(keySelected);
+  }, 150);
+});
 
+function test(keytest) {
+  let startIndex = buttonArray.indexOf(`${keytest}`) + 1;
+  let arr = [];
+  for (let i = startIndex; i < startIndex + 5; i++) {
+    arr.push(buttonArray[i]);
+  }
+  const light1 = document.querySelector(`div.padKey-${arr[0]}`);
+  const light2 = document.querySelector(`div.padKey-${arr[1]}`);
+  const light3 = document.querySelector(`div.padKey-${arr[2]}`);
+  const light4 = document.querySelector(`div.padKey-${arr[3]}`);
+  const light5 = document.querySelector(`div.padKey-${arr[4]}`);
+
+  light1.classList.add("playing");
+  setTimeout(() => {
+    clearTimeout(light1);
+    light1.classList.remove("playing");
+    light2.classList.add("playing");
+    setTimeout(() => {
+      clearTimeout(light2);
+      light2.classList.remove("playing");
+      light3.classList.add("playing");
+      setTimeout(() => {
+        clearTimeout(light3);
+        light3.classList.remove("playing");
+        light4.classList.add("playing");
+        setTimeout(() => {
+          clearTimeout(light4);
+          light4.classList.remove("playing");
+          light5.classList.add("playing");
+          setTimeout(() => {
+            clearTimeout(light5);
+            light5.classList.remove("playing");
+          }, 150);
+        }, 150);
+      }, 150);
+    }, 150);
+  }, 150);
+}
