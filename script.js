69766170454de28f46ed36a14ac751e4be8e0612
Davhho-dev <Davhho@utexas.edu>
2022-10-02 10:22:02 -0500

    Create arr for all pads that are in the row and columns

--- script.js
+++ script.js
@@ -1,91 +1,19 @@
-const buttonArray = [
-  "81",
-  "87",
-  "69",
-  "82",
-  "84",
-  "89",
-  "85",
-  "73",
-  "79",
-  "80",
-  "219",
-  "221",
-  "220",
-  "65",
-  "83",
-  "68",
-  "70",
-  "71",
-  "72",
-  "74",
-  "75",
-  "76",
-  "186",
-  "222",
-  "13",
-  "90",
-  "88",
-  "67",
-  "86",
-  "66",
-  "78",
-  "77",
-  "188",
-  "190",
-  "191",
-  "16",
-];
+const buttonArray = ["81", "87","69","82","84","89","85","73","79","80","219", "221","220","65","83","68","70","71","72","74","75", "76", "186", "222","13", "90", "88", "67", "86", "66","78",
+"77","188", "190", "191","16"];
 
-const buttonImg = [
-  "q",
-  "w",
-  "e",
-  "r",
-  "t",
-  "y",
-  "u",
-  "i",
-  "o",
-  "p",
-  "[",
-  "]",
-  "\\",
-  "a",
-  "s",
-  "d",
-  "f",
-  "g",
-  "h",
-  "j",
-  "k",
-  "l",
-  ";",
-  "'",
-  "Enter",
-  "z",
-  "x",
-  "c",
-  "v",
-  "b",
-  "n",
-  "m",
-  ",",
-  ".",
-  "/",
-  "Shift",
-];
+const buttonImg = ["q","w","e","r","t","y","u","i","o","p","[","]","\\","a","s","d","f","g","h",
+"j","k","l",";","'","Enter","z","x","c","v","b","n","m",",",".","/","Shift"];
 
 const arrowArray = ["left", "up", "right", "down"];
 const arrowImg = ["\u2190", "\u2191", "\u2192", "\u2193"];
 
-const arrowContainer = document.querySelector(".arrow-container");
-for (let i = 0; i < 4; i++) {
-  const arrowBtns = document.createElement("button");
-  arrowBtns.classList.add("arrow", `arrowBtns-${arrowArray[i]}`);
-  arrowBtns.textContent = arrowImg[i];
-  arrowContainer.append(arrowBtns);
-}
+// const arrowContainer = document.querySelector(".arrow-container");
+// for (let i = 0; i < 4; i++) {
+//   const arrowBtns = document.createElement("button");
+//   arrowBtns.classList.add("arrow", `arrowBtns-${arrowArray[i]}`);
+//   arrowBtns.textContent = arrowImg[i];
+//   arrowContainer.append(arrowBtns);
+// }
 
 // const buttonContainer = document.querySelector(".button-container");
 // for (let row = 0; row < 6; row++) {
@@ -115,21 +43,40 @@ window.addEventListener("keydown", (e) => {
   padSelected.classList.add("playing");
   setTimeout(function () {
     padSelected.classList.remove("playing");
-    test(keySelected);
+    lightSequence(keySelected);
   }, 150);
 });
 
-function test(keytest) {
-  let startIndex = buttonArray.indexOf(`${keytest}`) + 1;
+function mkPadArr(keys) {
+  let startIndex = buttonArray.indexOf(`${keys}`) + 1;
+  let endIndex = startIndex + 5;
   let arr = [];
-  for (let i = startIndex; i < startIndex + 5; i++) {
+  for(let i = startIndex; i < endIndex; i++) {
     arr.push(buttonArray[i]);
   }
+
+  // let indexMultiples = buttonArray.indexOf(`${keys}`);
+  // let indexMultiLength = (6 * 5) + indexMultiples;
+  // for(let j = indexMultiples + 6; j <= indexMultiLength; j+=6) {
+  //   arr.push(buttonArray[j]);
+  // }
+  // console.log(arr);
+
+  return arr;
+}
+
+function lightSequence(keys) {
+  let arr = mkPadArr(keys)
   const light1 = document.querySelector(`div.padKey-${arr[0]}`);
   const light2 = document.querySelector(`div.padKey-${arr[1]}`);
   const light3 = document.querySelector(`div.padKey-${arr[2]}`);
   const light4 = document.querySelector(`div.padKey-${arr[3]}`);
   const light5 = document.querySelector(`div.padKey-${arr[4]}`);
+  // const light6 = document.querySelector(`div.padKey-${arr[5]}`);
+  // const light7 = document.querySelector(`div.padKey-${arr[6]}`);
+  // const light8 = document.querySelector(`div.padKey-${arr[7]}`);
+  // const light9 = document.querySelector(`div.padKey-${arr[8]}`);
+  // const light10 = document.querySelector(`div.padKey-${arr[9]}`);
 
   light1.classList.add("playing");
   setTimeout(() => {
